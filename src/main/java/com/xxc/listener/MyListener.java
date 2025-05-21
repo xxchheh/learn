0fb2502ed7eba2209fa8b5e2b9df8b04462b8969
xuxincheng <2961166903@qq.com>
2025-05-21 10:02:40 +0800

    listener

---
+++ src/main/java/com/xxc/listener/MyListener.java
@@ -0,0 +1,41 @@
+package com.xxc.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class MyListener implements
+        ServletContextListener, HttpSessionListener, ServletRequestListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("contextInitialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed");
+    }
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        System.out.println("requestDestroyed");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        System.out.println("requestInitialized");
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("sessionCreated");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("sessionDestroyed");
+    }
+}
---
+++ src/main/webapp/myjsp.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AGiView
+  Date: 2025/5/21
+  Time: 08:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test listener</title>
+</head>
+<body>
+this is a listener page to test listener
+</body>
+</html>
